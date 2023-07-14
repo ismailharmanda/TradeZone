a0c929b7cebf96238d826dbd0d5eaf3ebffa3f5c
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-14 11:06:26 +0300

    fix: naming for ProductDetail screen

--- src/screens/ProductDetail/index.tsx
+++ src/screens/ProductDetail/index.tsx
@@ -8,25 +8,26 @@ import {
 } from './productDetail.action';
 import {GenericActionCreator} from 'utils';
 
-export const ProductsScreen = () => {
+export const ProductDetailScreen = () => {
   const dispatch = useDispatch();
 
-  const productsState = useSelector<GlobalState, ProductDetailState>(
+  const productDetailState = useSelector<GlobalState, ProductDetailState>(
     state => state.PRODUCT_DETAIL,
   );
-  console.log('productsState', productsState);
+  console.log('productDetailState', productDetailState);
 
   useEffect(() => {
     dispatch(
       GenericActionCreator({
         type: PRODUCT_DETAIL_ACTION_TYPES.PRODUCT_DETAIL_REQUEST,
+        payload: 1,
       }),
     );
   }, [dispatch]);
 
   return (
     <View>
-      <Text>Products Screen</Text>
+      <Text>Product Detail Screen</Text>
     </View>
   );
 };
