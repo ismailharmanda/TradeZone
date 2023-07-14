9f3bb4c923006eec26ada6d2324f153b38ef64ab
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-14 11:18:44 +0300

    chore: add detail saga to rootsaga

--- src/store/sagas/index.ts
+++ src/store/sagas/index.ts
@@ -1,9 +1,14 @@
 import LoginModuleSaga from 'screens/Login/login.saga';
 import ProductsModuleSaga from 'screens/Products/products.saga';
+import ProductDetailModuleSaga from 'screens/ProductDetail/productDetail.saga';
 import {all, fork, take} from 'redux-saga/effects';
 import {REHYDRATE} from 'redux-persist/lib/constants';
 
 export default function* rootSaga(): Generator {
   yield take(REHYDRATE);
-  yield all([fork(LoginModuleSaga), fork(ProductsModuleSaga)]);
+  yield all([
+    fork(LoginModuleSaga),
+    fork(ProductsModuleSaga),
+    fork(ProductDetailModuleSaga),
+  ]);
 }
