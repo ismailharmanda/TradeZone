17e664c66952a52db661eef9ed9758deea667091
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-14 11:20:11 +0300

    feat: import ProductDetail screen to App.tsx and see if it works perfectly

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {ProductsScreen} from 'screens/Products';
+import {ProductDetailScreen} from 'screens/ProductDetail';
 import {Provider} from 'react-redux';
 import {store, persistor} from 'store';
 import {PersistGate} from 'redux-persist/integration/react';
@@ -16,7 +16,10 @@ function App(): JSX.Element {
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
         <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen name={SCREENS.PRODUCTS} component={ProductsScreen} />
+            <Stack.Screen
+              name={SCREENS.PRODUCT_DETAIL}
+              component={ProductDetailScreen}
+            />
           </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
