dd98724e19ea1e8f7e83797dc92e4cbe5ed8d779
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-15 09:12:41 +0300

    feat: create a authenticated user navigator draft

---
+++ src/navigation/UserNavigator.tsx
@@ -0,0 +1,94 @@
+import React, {useMemo} from 'react';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {ProductsScreen} from 'screens/Products';
+import {ProductDetailScreen} from 'screens/ProductDetail';
+import {ProfileScreen} from 'screens/Profile';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {CartScreen} from 'screens/Cart';
+
+const Tab = createMaterialBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+export const UserNavigator = () => {
+  const ProductsStackScreen = useMemo(() => {
+    return () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="ProductsScreen" component={ProductsScreen} />
+          <Stack.Screen name="ProductDetail" component={ProductDetailScreen} />
+        </Stack.Navigator>
+      );
+    };
+  }, []);
+
+  const ProfileStackScreen = useMemo(() => {
+    return () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        </Stack.Navigator>
+      );
+    };
+  }, []);
+
+  const CartStackScreen = useMemo(() => {
+    return () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="CartScreen" component={CartScreen} />
+        </Stack.Navigator>
+      );
+    };
+  }, []);
+
+  return (
+    <Tab.Navigator
+      activeColor="#f0edf6"
+      inactiveColor="#f0edf6"
+      barStyle={{backgroundColor: '#F27919'}}>
+      <Tab.Screen
+        options={{
+          tabBarLabel: 'Products',
+          tabBarIcon: ({color, focused}) => (
+            <MaterialCommunityIcons
+              name="home"
+              color={focused ? '#F27919' : color}
+              size={26}
+            />
+          ),
+        }}
+        name="ProductsStack"
+        component={ProductsStackScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: 'Cart',
+          tabBarIcon: ({color, focused}) => (
+            <MaterialCommunityIcons
+              name="cart"
+              color={focused ? '#F27919' : color}
+              size={26}
+            />
+          ),
+        }}
+        name="CartStack"
+        component={CartStackScreen}
+      />
+      <Tab.Screen
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({color, focused}) => (
+            <MaterialCommunityIcons
+              name="menu"
+              color={focused ? '#F27919' : color}
+              size={26}
+            />
+          ),
+        }}
+        name="ProfileStack"
+        component={ProfileStackScreen}
+      />
+    </Tab.Navigator>
+  );
+};
