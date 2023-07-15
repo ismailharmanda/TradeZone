2d5ca12f36acc88272b78203456b8de6bee26a47
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-15 18:19:47 +0300

    chore: create a custom categories list

---
+++ src/components/CategoriesList.tsx
@@ -0,0 +1,58 @@
+import {useCallback} from 'react';
+import {
+  FlatList,
+  ViewStyle,
+  ListRenderItem,
+  View,
+  StyleSheet,
+} from 'react-native';
+import {Categories} from 'screens/Products/products';
+import {theme} from 'theme';
+
+interface Props {
+  data: Categories;
+  renderItem: ListRenderItem<string>;
+  ListEmptyComponent: JSX.Element;
+  contentContainerStyle?: ViewStyle;
+  listStyle?: ViewStyle;
+}
+
+export const CategoriesList = ({
+  data,
+  renderItem,
+  ListEmptyComponent,
+  contentContainerStyle,
+  listStyle,
+}: Props) => {
+  const keyExtractor = useCallback((item: any) => item, []);
+
+  const ItemSeperator = useCallback(
+    () => <View style={styles.itemSeperator} />,
+    [],
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        showsHorizontalScrollIndicator={false}
+        ItemSeparatorComponent={ItemSeperator}
+        horizontal
+        style={[listStyle]}
+        contentContainerStyle={contentContainerStyle}
+        keyExtractor={keyExtractor}
+        data={data}
+        renderItem={renderItem}
+        ListEmptyComponent={ListEmptyComponent}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: theme.spacing.xl,
+  },
+  itemSeperator: {
+    width: theme.spacing.md,
+  },
+});
