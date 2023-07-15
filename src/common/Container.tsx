d2d950e4fd018d18c1b280e10e5a3648f3b4a540
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-15 12:47:47 +0300

    chore: create a common container in order to use throughout the app

---
+++ src/common/Container.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {Image, StyleSheet, KeyboardAvoidingView, ViewStyle} from 'react-native';
+import {theme} from 'theme';
+
+type Props = {
+  children: React.ReactNode;
+  withLogo?: boolean;
+  containerStyle?: ViewStyle;
+};
+
+export const Container = ({
+  children,
+  withLogo = true,
+  containerStyle,
+}: Props) => (
+  <KeyboardAvoidingView
+    style={[styles.container, containerStyle]}
+    behavior="padding">
+    <Image
+      source={require('../assets/logo.png')}
+      resizeMode="contain"
+      style={[styles.logo, withLogo ? null : styles.hide]}
+    />
+    {children}
+  </KeyboardAvoidingView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    paddingHorizontal: theme.spacing.screenHorizontalPadding,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.colors.base.white,
+    gap: theme.spacing.md,
+  },
+  logo: {
+    width: '40%',
+    height: '20%',
+  },
+  hide: {
+    display: 'none',
+  },
+});
