3aba76e88452174e3b953a372ce09ffd07968eb4
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-16 13:40:30 +0300

    chore: create a function inorder to fix number without converting them to string

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -32,3 +32,8 @@ export const passwordValidator = (password: string) => {
 
   return '';
 };
+
+export const toFixedNumber = (num: number, digits: number, base?: number) => {
+  const pow = Math.pow(base ?? 10, digits);
+  return Math.round(num * pow) / pow;
+};
