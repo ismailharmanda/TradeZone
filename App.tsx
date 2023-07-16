5ce4ef70b309313bf6788757a9617b2a740e2ae8
İsmail Harmanda <iharmanda@hotmail.com>
2023-07-16 19:43:15 +0300

    feat: add splash screen, change app logo

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {store, persistor} from 'store';
 import {PersistGate} from 'redux-persist/integration/react';
 import {Navigation} from 'navigation/navigation';
 import Toast from 'react-native-toast-message';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <Provider store={store}>
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
--- android/app/src/main/java/com/tradezone/MainActivity.java
+++ android/app/src/main/java/com/tradezone/MainActivity.java
@@ -1,9 +1,10 @@
 package com.tradezone;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
